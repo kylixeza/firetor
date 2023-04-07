b896d62564252c8cc9a72a9fc4ab0b9d3295b597
ACER <loilyxtra@gmail.com>
2023-04-07 10:25:03 +0700

    dependency: added ktor, firebase, kotlin dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    kotlin("jvm") version "1.8.20"
 }
 
 group = "com.kylix"
@@ -12,8 +13,16 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(kotlin("stdlib-jdk8"))
+
+    implementation("io.ktor:ktor-server-core-jvm:2.2.4")
+    implementation("org.slf4j:slf4j-simple:2.0.5")
+    implementation("com.google.firebase:firebase-admin:9.1.1")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(11)
 }
