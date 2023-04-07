10a7d93d5f2a2e9419d87a4edd206e6f9213dcb7
ACER <loilyxtra@gmail.com>
2023-04-07 10:39:52 +0700

    dependency: added jnanoid dependency and implement core module

--- storage/build.gradle.kts
+++ storage/build.gradle.kts
@@ -14,10 +14,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
+    implementation("com.aventrix.jnanoid:jnanoid:2.0.0")
     implementation("io.ktor:ktor-server-core-jvm:2.2.4")
     implementation("org.slf4j:slf4j-simple:2.0.5")
     implementation("com.google.firebase:firebase-admin:9.1.1")
     implementation(kotlin("stdlib-jdk8"))
+
+    implementation(project(":core"))
 }
 
 tasks.test {
