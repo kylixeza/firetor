d63e61a0bdfc76c4aef0530fe6ecfd6db3efa7ef
ACER <loilyxtra@gmail.com>
2023-04-07 11:11:32 +0700

    dependency: added core and storage module

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,4 +26,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation(project(":core"))
+    implementation(project(":storage"))
 }
