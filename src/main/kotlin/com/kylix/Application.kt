fead5cde573808e3b5f084dcbf98586682d82774
ACER <loilyxtra@gmail.com>
2023-04-07 11:12:38 +0700

    test: firetor plugin with real image

--- src/main/kotlin/com/kylix/Application.kt
+++ src/main/kotlin/com/kylix/Application.kt
@@ -6,10 +6,11 @@ import io.ktor.server.netty.*
 import com.kylix.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "localhost", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
+    configureFiretor()
     configureRouting()
 }
--- src/main/kotlin/com/kylix/plugins/Routing.kt
+++ src/main/kotlin/com/kylix/plugins/Routing.kt
@@ -1,13 +1,55 @@
 package com.kylix.plugins
 
+import com.kylix.FirebaseStorageImage.uploadImage
+import com.kylix.ImageExtension
+import io.ktor.http.content.*
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        post("/image") {
+            val multipart = call.receiveMultipart()
+            var url: String? = null
+
+            try {
+                multipart.forEachPart {part ->
+                    if (part is PartData.FileItem) {
+                        url = part.uploadImage()
+                    }
+                }
+                call.respond(url ?: "No image uploaded")
+            } catch (e: Exception) {
+                call.respondText("Error: ${e.message}")
+            }
+        }
+
+        post("/image-with-params") {
+            val multipart = call.receiveMultipart()
+            var url: String? = null
+
+            try {
+                multipart.forEachPart {part ->
+                    if (part is PartData.FileItem) {
+                        url = part.uploadImage(
+                            path = "images",
+                            fileExtension = ImageExtension.PNG,
+                            preprocessing = {
+                                it.compress(0.1f)
+                            }
+                        )
+                    }
+                }
+                call.respond(url ?: "No image uploaded")
+            } catch (e: Exception) {
+                call.respondText("Error: ${e.message}")
+            }
         }
     }
 }
