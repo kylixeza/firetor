1fc8e491e8a7ef8e2ca837f375a944bff6f99d81
ACER <loilyxtra@gmail.com>
2023-04-15 18:09:53 +0700

    feat: added standard name for file name that contains space

--- storage/src/main/java/com/kylix/URLBuilder.kt
+++ storage/src/main/java/com/kylix/URLBuilder.kt
@@ -7,10 +7,11 @@ object URLBuilder {
     internal fun String.reference(ref: String) = "$this$ref%2F"
 
     internal fun String.getDownloadUrl(fileName: String) = run {
+        val standardizedName = fileName.replace(" ", "%20")
         if(this.isEmpty()) {
-            return@run "https://firebasestorage.googleapis.com/v0/b/${Configuration.getStorageBucket()}/o/$fileName?alt=media"
+            return@run "https://firebasestorage.googleapis.com/v0/b/${Configuration.getStorageBucket()}/o/$standardizedName?alt=media"
         } else {
-            return@run "https://firebasestorage.googleapis.com/v0/b/${Configuration.getStorageBucket()}/o/$this$fileName?alt=media"
+            return@run "https://firebasestorage.googleapis.com/v0/b/${Configuration.getStorageBucket()}/o/$this$standardizedName?alt=media"
         }
     }
 
