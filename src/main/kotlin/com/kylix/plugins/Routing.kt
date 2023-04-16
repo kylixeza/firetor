c62180f578cbf2963cdf629ab33ee24f0868ed28
ACER <loilyxtra@gmail.com>
2023-04-16 11:56:34 +0700

    ref: removed unuse import

--- src/main/kotlin/com/kylix/plugins/Routing.kt
+++ src/main/kotlin/com/kylix/plugins/Routing.kt
@@ -1,13 +1,9 @@
 package com.kylix.plugins
 
-import com.kylix.image.FirebaseStorageImage.uploadImage
-import com.kylix.image.ImageExtension
 import com.kylix.routes.postImage
-import io.ktor.http.content.*
-import io.ktor.server.routing.*
-import io.ktor.server.response.*
 import io.ktor.server.application.*
-import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
